2c4cc3fc87cbc4e565a882da431980ca7a58fc8c
venkat <venkat.prabhu@10decoders.in>
2024-03-22 12:54:30 +0530

    angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstDirective } from './shared/directive/first.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstDirective
   ],
   imports: [
     BrowserModule
