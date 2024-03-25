996ccb3c60708ffc8aceb81d327bb86e959aeee9
venkat <venkat.prabhu@10decoders.in>
2024-03-25 11:37:37 +0530

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstDirective } from './shared/directive/first.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FirstDirective } from './shared/directive/first.directive';
     FirstDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
