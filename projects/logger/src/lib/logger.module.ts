5a8f1176f170c51e4eed0fd6c03bbfeff8d6020a
venkat <venkat.prabhu@10decoders.in>
2024-03-26 10:35:24 +0530

    libraries and projects

---
+++ projects/logger/src/lib/logger.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { LoggerComponent } from './logger.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoggerComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LoggerComponent
+  ]
+})
+export class LoggerModule { }
